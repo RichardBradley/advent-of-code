1a7ef57e2af92adf41e09dda671b126c9f103d2f
Richard Bradley <Richard.Bradley@softwire.com>
2024-12-02 09:13:55 +0000

    Y2024D02

---
+++ src/main/java/y2024/Y2024D01.java
@@ -0,0 +1,91 @@
+package y2024;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.io.Resources;
+import org.junit.Assert;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class Y2024D01 {
+
+    public static void main(String[] args) throws Exception {
+        Stopwatch sw = Stopwatch.createStarted();
+        List<String> input = Resources.readLines(Resources.getResource("y2024/Y2024D01.txt"), StandardCharsets.UTF_8);
+
+        // 1
+        assertThat(part1(example)).isEqualTo(11);
+        System.out.println(part1(input));
+
+        // 2
+        assertThat(part2(example)).isEqualTo(31);
+        System.out.println(part2(input));
+
+        System.out.println("Took " + sw.elapsed(TimeUnit.MILLISECONDS) + "ms");
+    }
+
+    private static long part1(List<String> input) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+
+        Pattern p = Pattern.compile("^(\\d+) +(\\d+)$");
+        for (String line : input) {
+            Matcher m = p.matcher(line);
+            Assert.assertTrue(m.matches());
+
+            l1.add(Integer.parseInt(m.group(1)));
+            l2.add(Integer.parseInt(m.group(2)));
+        }
+
+        l1.sort(Comparator.naturalOrder());
+        l2.sort(Comparator.naturalOrder());
+
+        assertThat(l1.size()).isEqualTo(l2.size());
+
+        long totalDist = 0;
+        for (int i = 0; i < l1.size(); i++) {
+            totalDist += Math.abs(l1.get(i) - l2.get(i));
+        }
+        return totalDist;
+    }
+
+    private static long part2(List<String> input) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+
+        Pattern p = Pattern.compile("^(\\d+) +(\\d+)$");
+        for (String line : input) {
+            Matcher m = p.matcher(line);
+            Assert.assertTrue(m.matches());
+
+            l1.add(Integer.parseInt(m.group(1)));
+            l2.add(Integer.parseInt(m.group(2)));
+        }
+
+        assertThat(l1.size()).isEqualTo(l2.size());
+
+        long acc = 0;
+        for (int i = 0; i < l1.size(); i++) {
+            int x = l1.get(i);
+            long count = l2.stream().filter(y -> x == y).count();
+            acc += x * count;
+        }
+        return acc;
+    }
+
+    static List<String> example = List.of(
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3"
+    );
+}
---
+++ src/main/java/y2024/Y2024D02.java
@@ -0,0 +1,118 @@
+package y2024;
+
+import com.google.common.base.Splitter;
+import com.google.common.base.Stopwatch;
+import com.google.common.io.Resources;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class Y2024D02 {
+
+    public static void main(String[] args) throws Exception {
+        Stopwatch sw = Stopwatch.createStarted();
+        List<String> input = Resources.readLines(Resources.getResource("y2024/Y2024D02.txt"), StandardCharsets.UTF_8);
+
+        // 1
+        assertThat(part1(example)).isEqualTo(2);
+        assertThat(part1(input)).isEqualTo(407);
+
+        // 2
+        assertThat(part2(example)).isEqualTo(4);
+        assertThat(part2(input)).isEqualTo(459);
+
+        System.out.println("Took " + sw.elapsed(TimeUnit.MILLISECONDS) + "ms");
+    }
+
+    private static long part1(List<String> input) {
+        int safeCount = 0;
+
+        Splitter s = Splitter.on(' ');
+        for (String line : input) {
+            List<Integer> vals = s.splitToList(line).stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            if (isSafe(vals)) {
+                safeCount++;
+            }
+        }
+
+        return safeCount;
+    }
+
+    private static long part2(List<String> input) {
+        int safeCount = 0;
+
+        Splitter s = Splitter.on(' ');
+        lines:
+        for (String line : input) {
+            List<Integer> vals = s.splitToList(line).stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            if (isSafe(vals)) {
+                safeCount++;
+            } else {
+                for (int i = 0; i < vals.size(); i++) {
+                    ArrayList<Integer> vals2 = new ArrayList<>(vals);
+                    vals2.remove(i);
+                    if (isSafe(vals2)) {
+                        safeCount++;
+                        continue lines;
+                    }
+                }
+            }
+        }
+
+        return safeCount;
+    }
+
+    private static boolean isSafe(List<Integer> vals) {
+
+        boolean isAsc;
+        if (vals.get(0) < vals.get(1)) {
+            isAsc = true;
+        } else if (vals.get(0) > vals.get(1)) {
+            isAsc = false;
+        } else {
+            return false;
+        }
+
+        for (int i = 0; i < vals.size() - 1; i++) {
+            // The levels are either all increasing or all decreasing.
+            int a = vals.get(i);
+            int b = vals.get(i + 1);
+
+            if (isAsc) {
+                if (!(a < b)) {
+                    return false;
+                }
+            } else {
+                if (!(a > b)) {
+                    return false;
+                }
+            }
+
+            // Any two adjacent levels differ by at least one and at most three.
+            int diff = Math.abs(a - b);
+            if (diff < 1 || diff > 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static List<String> example = List.of(
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
+    );
+}
